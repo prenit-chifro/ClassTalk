949ab25fc0cbe54ea9dd6aa941d92f134b1b7d1f
prenit-chifro <prenit.wankhede@chifro.com>
2016-11-25 15:53:16 +0530

    final

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -11,8 +11,16 @@ class GradesController < ApplicationController
 	end
 
 	def index
-		@grades = current_user.members_sections.map{|m| m.grade}.uniq
-	end
+		if(current_user.role == "Student" or current_user.role == "Parent")
+			@grades = current_user.members_sections.map{|m| m.grade}.uniq
+		end
+		if(@current_user.role == "Teacher")
+			@grade = current_user.teaching_sections_subjects_models.map(&:grade).uniq
+		end
+		if(current_user.role == "Principal" or current_user.role == "Institute Admin")
+			@grades = @institute.grades
+		end
+	end	
 
 	def new
 
@@ -23,8 +31,19 @@ class GradesController < ApplicationController
 	end
 
 	def show
-		@section_member_models = current_user.members_sections
-		@grade_section_models = @grade.grades_sections.where(institute_id: @institute.id, section_id: @section_member_models.map(&:section_id))
+		if(current_user.role == "Student" or current_user.role == "Parent")
+			@section_member_models = current_user.members_sections
+			@grade_section_models = @grade.grades_sections.where(institute_id: @institute.id, section_id: @section_member_models.map(&:section_id))
+		end
+		if(@current_user.role == "Teacher")
+			@teaching_section_subject_models = current_user.teaching_sections_subjects_models
+			@grade_section_models = @grade.grades_sections.where(institute_id: @institute.id, section_id: @teaching_section_subject_models.map(&:section_id))
+		end
+		if(current_user.role == "Principal" or current_user.role == "Institute Admin")
+			@grade_section_models = @grade.grades_sections.where(institute_id: @institute.id)
+		end
+
+		
 	end
 
 	def edit
